e9cf54b47df2a65ee5cd1fb55fe5cc8426b2e2aa
zhkong <klarkzh@hotmail.com>
2023-08-14 03:40:24 +0800

    修改包地址

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -2,7 +2,7 @@
 # @Author: zhkong
 # @Date: 2023-07-25 17:07:02
  # @LastEditors: zhkong
- # @LastEditTime: 2023-08-13 03:28:50
+ # @LastEditTime: 2023-08-14 02:16:43
  # @FilePath: /xiaomi-ax3600-openwrt-build/scripts/prepare.sh
 ###
 
@@ -30,16 +30,19 @@ git clone https://github.com/vernesong/OpenClash.git --single-branch --depth 1 p
 ## argon theme
 git clone https://github.com/jerrykuku/luci-theme-argon.git --single-branch --depth 1 package/new/luci-theme-argon
 ## KMS激活
-git clone https://github.com/flytosky-f/openwrt-vlmcsd.git --single-branch --depth 1 package/new/vlmcsd
-git clone https://github.com/ssuperh/luci-app-vlmcsd-new.git --single-branch --depth 1 package/new/luci-app-vlmcsd-new
-## Adguard Home
-svn export https://github.com/immortalwrt/packages/branches/master/net/adguardhome package/new/adguardhome
+svn export https://github.com/immortalwrt/luci/branches/master/applications/luci-app-vlmcsd package/new/luci-app-vlmcsd
+svn export https://github.com/immortalwrt/packages/branches/master/net/vlmcsd package/new/vlmcsd
+# edit package/new/luci-app-vlmcsd/Makefile
+sed -i 's/..\/..\/luci.mk/$(TOPDIR)\/feeds\/luci\/luci.mk/g' package/new/luci-app-vlmcsd/Makefile
+
+# ## Adguard Home
+# svn export https://github.com/immortalwrt/packages/branches/openwrt-23.05/net/adguardhome package/new/adguardhome
 ## mosdns
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/new/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/new/v2ray-geodata
 
 # AutoCore
-svn export https://github.com/zhkong/openwrt-pkg/branches/master/autocore package/new/autocore
+svn export https://github.com/immortalwrt/immortalwrt/branches/master/package/emortal/autocore package/new/autocore
 sed -i 's/"getTempInfo" /"getTempInfo", "getCPUBench", "getCPUUsage" /g' package/new/autocore/files/luci-mod-status-autocore.json
 
 rm -rf feeds/luci/modules/luci-base
@@ -47,10 +50,10 @@ rm -rf feeds/luci/modules/luci-mod-status
 rm -rf feeds/packages/utils/coremark
 rm -rf package/emortal/default-settings
 
-svn export https://github.com/immortalwrt/luci/branches/openwrt-23.05/modules/luci-base feeds/luci/modules/luci-base
-svn export https://github.com/immortalwrt/luci/branches/openwrt-23.05/modules/luci-mod-status feeds/luci/modules/luci-mod-status
-svn export https://github.com/immortalwrt/packages/branches/openwrt-23.05/utils/coremark feeds/packages/utils/coremark
-svn export https://github.com/immortalwrt/immortalwrt/branches/openwrt-23.05/package/emortal/default-settings package/emortal/default-settings
+svn export https://github.com/immortalwrt/luci/branches/master/modules/luci-base feeds/luci/modules/luci-base
+svn export https://github.com/immortalwrt/luci/branches/master/modules/luci-mod-status feeds/luci/modules/luci-mod-status
+svn export https://github.com/immortalwrt/packages/branches/master/utils/coremark package/new/coremark
+svn export https://github.com/immortalwrt/immortalwrt/branches/master/package/emortal/default-settings package/emortal/default-settings
 # svn export https://github.com/immortalwrt/immortalwrt/branches/openwrt-23.05/package/utils/mhz package/utils/mhz
 
 # 修复Architecture显示错误问题
