f92dfe7753d8dbdb86dfbd538c4e171f77149693
zhkong <klarkzh@hotmail.com>
2023-12-30 12:46:09 +0800

    适配最新代码

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -2,7 +2,7 @@
 # @Author: zhkong
 # @Date: 2023-07-25 17:07:02
  # @LastEditors: zhkong
- # @LastEditTime: 2023-12-04 12:24:40
+ # @LastEditTime: 2023-12-30 12:45:26
  # @FilePath: /xiaomi-ax3600-openwrt-build/scripts/prepare.sh
 ###
 
@@ -12,8 +12,7 @@ cd openwrt
 # 增加ax3600 stock布局
 git remote add upstream https://github.com/zhkong/openwrt-ipq807x.git
 git fetch upstream xiaomi-ax3600-stock-layout --depth 3
-git cherry-pick e2bb765
-git cherry-pick 6f6eb1d
+git cherry-pick f365886
 #如果checkout失败，说明有冲突，停止编译
 if [ $? -ne 0 ]; then
     echo "cherry-pick failed, please check"
@@ -54,8 +53,8 @@ svn export https://github.com/immortalwrt/packages/branches/master/utils/coremar
 svn export https://github.com/immortalwrt/immortalwrt/branches/master/package/emortal/default-settings package/emortal/default-settings
 # svn export https://github.com/immortalwrt/immortalwrt/branches/openwrt-23.05/package/utils/mhz package/utils/mhz
 
-# fix luci-theme-argon css
-bash ../scripts/fix-argon.sh
+# # fix luci-theme-argon css
+# bash ../scripts/fix-argon.sh
 
 # 增加 oh-my-zsh
 bash ../scripts/preset-terminal-tools.sh
